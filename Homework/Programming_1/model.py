c40ad9691a77aacd1ef98abd70b9060a7597fedc
MighTy-Weaver <1874240442@qq.com>
2022-10-02 16:51:32 +0800

    update

---
+++ Homework/Programming_1/model.py
@@ -0,0 +1,17 @@
+from torch import nn
+from torch import sigmoid
+from torch.nn import Linear
+
+
+class LogisticRegression_Classifier(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegression_Classifier, self).__init__()
+        self.linear = Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        outputs = sigmoid(self.linear(x))
+        return outputs
+
+class SVM_Classifier(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SVM_Classifier,self).__init__()
---
+++ Homework/Programming_1/train_all_LR.py
@@ -0,0 +1,8 @@
+import os
+
+lr_choices = [5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5]
+opt_choices = ['SGD', 'SGDm']
+
+for lr in lr_choices:
+    for opt in opt_choices:
+        os.system("python LR_train.py --opt {} --lr {}".format(opt, lr))

0a301736fa5f9bb9358436a34eaccf21f879a14f
MighTy-Weaver <1874240442@qq.com>
2022-10-26 14:28:07 +0800

    update

---
+++ Homework/Programming_2/hw2.py
@@ -0,0 +1,30 @@
+import torch
+import torch.utils.data as td
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+def cifar_loaders(batch_size, shuffle_test=False):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.225, 0.225, 0.225])
+    train = datasets.CIFAR10('./', train=True, download=True,
+                             transform=transforms.Compose([
+                                 transforms.RandomHorizontalFlip(),
+                                 transforms.RandomCrop(32, 4),
+                                 transforms.ToTensor(),
+                                 normalize,
+                             ]))
+    test = datasets.CIFAR10('./', train=False,
+                            transform=transforms.Compose([transforms.ToTensor(), normalize]))
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+                                               shuffle=True, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size,
+                                              shuffle=shuffle_test, pin_memory=True)
+    return train_loader, test_loader
+
+
+batch_size = 64
+test_batch_size = 64
+
+train_loader, _ = cifar_loaders(batch_size)
+_, test_loader = cifar_loaders(test_batch_size)

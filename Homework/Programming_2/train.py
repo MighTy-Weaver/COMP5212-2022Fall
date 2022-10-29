5606ded2c7f0371577b05bf61797dd2ff39a6688
MighTy-Weaver <1874240442@qq.com>
2022-10-30 01:28:38 +0800

    update

--- Homework/Programming_2/train.py
+++ Homework/Programming_2/train.py
@@ -7,7 +7,9 @@ import torch
 import torch.utils.data as td
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torch.nn import CrossEntropyLoss
 from torch.optim import AdamW
+from tqdm import tqdm
 
 from model import CNN_Classifier
 from model import MLP_Classifier
@@ -73,6 +75,75 @@ else:
     model = CNN_Classifier(activation=args.activation).to(device)
 
 optimizer = AdamW(model.parameters(), lr=args.lr)
+loss = CrossEntropyLoss()
 save_path = f"./{args.model}_{args.activation}_{args.lr}_{args.epoch}/"
+
 if not os.path.exists(save_path):
     os.mkdir(save_path)
+
+progress_bar = tqdm(range(args.epoch * len(train_loader)))
+
+# Training the Model
+# Notice that newest Pytorch merge tensor and variable, so the additional Variable wrapping is no longer required.
+record_dict = {'trn_loss': [], 'val_loss': [], 'trn_acc': [], 'val_acc': [], 'trn_iter_loss': [], 'val_iter_loss': []}
+for epoch in range(1, 1 + args.epoch):
+    total_loss = 0
+    model.train()
+    trn_total_pred, trn_total_label = 0, 0
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(images.view(-1, 28 * 28))
+        labels = Variable(2 * (labels.float() - 0.5))
+
+        outputs = model(images).squeeze(1)
+        predicted_answer = sign(outputs).round().detach().cpu()
+        predicted_answer[predicted_answer == 0] = -1
+        loss = loss_func(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        record_dict['trn_iter_loss'].append(loss.item())
+
+        truth_answer = labels.detach().cpu()
+        trn_total_pred += (predicted_answer.view(-1).long() == labels).sum()
+        trn_total_label += images.shape[0]
+
+        progress_bar.update(1)
+
+    trn_acc = float(100 * trn_total_pred.float() / trn_total_label)
+    trn_loss = total_loss / len(train_loader)
+    record_dict['trn_acc'].append(trn_acc)
+    record_dict['trn_loss'].append(trn_loss)
+
+    # Test the Model
+    model.eval()
+    correct = 0.
+    total = 0.
+    eval_total_loss = 0
+
+    for images, labels in test_loader:
+        images = Variable(images.view(-1, 28 * 28))
+
+        outputs = model(images).squeeze(1)
+        prediction = sign(outputs).round().detach().cpu()
+
+        loss = loss_func(outputs, labels)
+        eval_total_loss += loss.item()
+        record_dict['val_iter_loss'].append(loss.item())
+
+        correct += (prediction.view(-1).long() == labels).sum()
+        total += images.shape[0]
+    eval_loss = eval_total_loss / len(test_loader)
+    eval_acc = float(100 * correct.float() / total)
+    record_dict['val_acc'].append(eval_acc)
+    record_dict['val_loss'].append(eval_loss)
+    progress_bar.set_postfix({
+        'trn_acc': trn_acc,
+        'val_acc': eval_acc
+    })
+    print('Accuracy of the model on train images: %f%% \t test images: %f%%' % (trn_acc, eval_acc))
+
+np.save('{}/record.npy'.format(save_path), record_dict)
+torch.save(model.state_dict(), '{}/ckpt.pt'.format(save_path))

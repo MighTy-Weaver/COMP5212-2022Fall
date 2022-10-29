047af0c8ebcc64ec75a77b29d05de822d940a1a2
MighTy-Weaver <1874240442@qq.com>
2022-10-30 01:18:28 +0800

    update

--- Homework/Programming_2/train.py
+++ Homework/Programming_2/train.py
@@ -7,9 +7,15 @@ import torch
 import torch.utils.data as td
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torch.optim import AdamW
+
+from model import CNN_Classifier
+from model import MLP_Classifier
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--gpu", default=0, type=int, help="Index of GPU to use")
+parser.add_argument("--activation", default='relu', type=str, help="Type of activation function",
+                    choices=['relu', 'sigmoid', 'softmax'])
 parser.add_argument("--model", type=str, choices=['MLP', 'CNN'], default='MLP', help="Model structure to use")
 parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate")
 parser.add_argument("--epoch", default=50, type=int, help="Number of epochs to be trained")
@@ -60,3 +66,13 @@ test_batch_size = 64
 
 train_loader, _ = cifar_loaders(batch_size)
 _, test_loader = cifar_loaders(test_batch_size)
+
+if args.model == 'MLP':
+    model = MLP_Classifier(activation=args.activation).to(device)
+else:
+    model = CNN_Classifier(activation=args.activation).to(device)
+
+optimizer = AdamW(model.parameters(), lr=args.lr)
+save_path = f"./{args.model}_{args.activation}_{args.lr}_{args.epoch}/"
+if not os.path.exists(save_path):
+    os.mkdir(save_path)

07a85ffef837a79dc97fed6c062e8231a0e47390
MighTy-Weaver <1874240442@qq.com>
2022-11-04 16:58:00 +0800

    update

--- Programming_Homework2/evaluate.py
+++ Programming_Homework2/evaluate.py
@@ -12,11 +12,11 @@ for a in activation_choices:
                                      round(max(CNN_record['val_acc']), 2)))
 
 plt.rcParams.update({'font.size': 16})
-plt.rcParams['figure.figsize'] = 20, 14
+plt.rcParams['figure.figsize'] = 21, 20
 for i in range(4):
     plt.subplot(2, 2, i + 1)
 
-plt.subplot(2, 2, 1)
+plt.subplot(3, 2, 1)
 for a in activation_choices:
     MLP_record = np.load(f'./MLP_{a}_0.001_50_record.npy', allow_pickle=True).item()
     plt.plot(range(len(MLP_record['trn_loss'])), MLP_record['trn_loss'], label=activation_str_dict[a])
@@ -25,7 +25,7 @@ plt.xlabel("Epoch")
 plt.ylabel("Training Loss")
 plt.title("Training Loss Curve for Multi Layer Perceptron")
 
-plt.subplot(2, 2, 2)
+plt.subplot(3, 2, 2)
 for a in activation_choices:
     CNN_record = np.load(f'./CNN_{a}_0.001_50_record.npy', allow_pickle=True).item()
     plt.plot(range(len(CNN_record['trn_loss'])), CNN_record['trn_loss'], label=activation_str_dict[a])
@@ -34,7 +34,26 @@ plt.xlabel("Epoch")
 plt.ylabel("Training Loss")
 plt.title("Training Loss Curve for Convolutional Neural Network")
 
-plt.subplot(2, 2, 3)
+plt.subplot(3, 2, 3)
+for a in activation_choices:
+    MLP_record = np.load(f'./MLP_{a}_0.001_50_record.npy', allow_pickle=True).item()
+    plt.plot(range(len(MLP_record['trn_acc'])), MLP_record['trn_acc'], label=activation_str_dict[a])
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Training Accuracy")
+plt.title("Training Accuracy Curve for Multi Layer Perceptron")
+
+plt.subplot(3, 2, 4)
+for a in activation_choices:
+    CNN_record = np.load(f'./CNN_{a}_0.001_50_record.npy', allow_pickle=True).item()
+    plt.plot(range(len(CNN_record['trn_acc'])), CNN_record['trn_acc'], label=activation_str_dict[a])
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Training Accuracy")
+plt.title("Training Accuracy Curve for Convolutional Neural Network")
+
+
+plt.subplot(3, 2, 5)
 for a in activation_choices:
     MLP_record = np.load(f'./MLP_{a}_0.001_50_record.npy', allow_pickle=True).item()
     plt.plot(range(len(MLP_record['val_acc'])), MLP_record['val_acc'], label=activation_str_dict[a])
@@ -43,7 +62,7 @@ plt.xlabel("Epoch")
 plt.ylabel("Testing Accuracy")
 plt.title("Testing Accuracy Curve for Multi Layer Perceptron")
 
-plt.subplot(2, 2, 4)
+plt.subplot(3, 2, 6)
 for a in activation_choices:
     CNN_record = np.load(f'./CNN_{a}_0.001_50_record.npy', allow_pickle=True).item()
     plt.plot(range(len(CNN_record['val_acc'])), CNN_record['val_acc'], label=activation_str_dict[a])
